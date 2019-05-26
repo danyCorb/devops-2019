4b18c5fe1eca8f4467724a828fbf9235533c914b
dany <danycor2@hotmail.fr>
2019-05-26 19:31:23 +0000

    Allerting

--- aubonbeurre/aubonbeurre-dataviz/src/App.vue
+++ aubonbeurre/aubonbeurre-dataviz/src/App.vue
@@ -1,5 +1,10 @@
 <template>
   <div id="app">
+    <h1>Au Bon Beurre - Data Visualizer</h1>
+    <li id="routing-list">
+      <router-link to="/data-visualizer">Unit Graphs</router-link>
+      <router-link to="/alertes">Alertes</router-link>
+    </li>
     <router-view />
   </div>
 </template>
@@ -24,6 +29,33 @@ export default {
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
+  margin-top: 10px;
 }
+
+#routing-list {
+  display: flex;
+  flex-direction: row;
+  justify-content: center;
+}
+
+#routing-list a {
+  margin: 5px;
+  padding: 6px;
+  background-color: mediumseagreen;
+  color: #fff;
+  text-decoration: none;
+  transition: background-color 200ms, box-shadow 200ms;
+}
+
+#routing-list a.router-link-active {
+  background-color: rgb(40, 124, 78);
+}
+
+
+#routing-list a:hover {
+  background-color: rgb(40, 124, 78);
+  box-shadow: 0 0 3px black;
+}
+
+
 </style>
--- aubonbeurre/aubonbeurre-dataviz/src/router.js
+++ aubonbeurre/aubonbeurre-dataviz/src/router.js
@@ -2,6 +2,7 @@ import Router from 'vue-router'
 import Vue from 'vue'
 
 import DataVisualizer from './view/data-visualizer.page'
+import AlertingPage from './view/alerting.vue'
 
 Vue.use(Router)
 
@@ -10,5 +11,6 @@ export default new Router({
     base: process.env.BASE_URL,
     routes: [
         { path: '/data-visualizer', component: DataVisualizer },
+        { path: '/alertes', component: AlertingPage },
     ]
 })
