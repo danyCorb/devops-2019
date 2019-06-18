1d396e033544e30b2cca64c6726dcbf7dacb725b
CorentinDupont <dupont.corentin44@gmail.com>
2019-06-18 13:12:32 +0200

    fix create users script

--- database/root-create-users.sql
+++ database/root-create-users.sql
@@ -4,13 +4,13 @@ DROP USER IF EXISTS 'delegate_administrator'@'%';
 CREATE USER 'delegate_administrator'@'%' IDENTIFIED BY 'delAdmPass44';
 
 GRANT ALL PRIVILEGES ON au_bon_beurre TO 'delegate_administrator'@'%';
-GRANT ALL PRIVILEGES on au_bon_beurre.* to 'delegate_administrator'@'%';
+GRANT ALL PRIVILEGES ON au_bon_beurre.* to 'delegate_administrator'@'%';
 
 -- create docker concentrator
 DROP USER IF EXISTS 'docker_concentrator'@'%';
 CREATE USER 'docker_concentrator'@'%' IDENTIFIED BY 'docConPass44';
 
-GRANT SELECT, CREATE ON au_bon_beurre.* TO 'delegate_administrator'@'%';
+GRANT SELECT, INSERT ON au_bon_beurre.* TO 'docker_concentrator'@'%';
 
 -- create data vision
 DROP USER IF EXISTS 'data_vision'@'%';
