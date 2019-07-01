e5a669cdde6244fcdcfc334e80181f532fd0421c
DanyCorbineau <danycor2@hotmail.fr>
2019-07-01 15:37:52 +0200

    forget mail, change by exception

--- script/unit/abbemail.py
+++ script/unit/abbemail.py
@@ -4,18 +4,22 @@ from email.mime.multipart import MIMEMultipart
 from email.mime.text import MIMEText
 
 def sendEmail(msgBody):
-    msg = MIMEMultipart()
-    msg["From"] = "data-au-bon-beurre@dany-corbineau.fr"
-    msg["To"] = "danycor2@hotmail.fr"
-    msg["Subject"] = "Au Bon Beurre - Error"
-    msg.attach(MIMEText(msgBody, 'plain'))
-    server = smtplib.SMTP("emailing",1025)
-    #server = smtplib.SMTP("smtp.office365.com",587)
-    #server.starttls()
-    server.ehlo_or_helo_if_needed()
-    #server.login("dany.corbineau@ynov.com", password)
-    try:
-        failed = server.sendmail("data-au-bon-beurre@dany-corbineau.fr","danycor2@hotmail.fr", msg.as_string())
-        server.close()
-    except Exception as e:
-        print(e)
+    try {
+        msg = MIMEMultipart()
+        msg["From"] = "data-au-bon-beurre@dany-corbineau.fr"
+        msg["To"] = "danycor2@hotmail.fr"
+        msg["Subject"] = "Au Bon Beurre - Error"
+        msg.attach(MIMEText(msgBody, 'plain'))
+        server = smtplib.SMTP("emailing",1025)
+        #server = smtplib.SMTP("smtp.office365.com",587)
+        #server.starttls()
+        server.ehlo_or_helo_if_needed()
+        #server.login("dany.corbineau@ynov.com", password)
+        try:
+            failed = server.sendmail("data-au-bon-beurre@dany-corbineau.fr","danycor2@hotmail.fr", msg.as_string())
+            server.close()
+        except Exception as e:
+            print(e)
+    }
+    except Exception  as e:
+        print('Mail Error')
